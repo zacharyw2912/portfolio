0dc7d6cd215c8be868a62798311ed90866d5d831
Zachary <zachary.white2912@gmail.com>
2025-02-23 21:02:25 +0000

    update

---
+++ main.js
@@ -0,0 +1,42 @@
+let lastScrollTop = 0;
+window.addEventListener("scroll", function() {
+    let nav = document.querySelector("nav");
+    let scrollTop = window.scrollY;
+    
+    if (scrollTop > lastScrollTop) {
+        nav.classList.add("hidden"); // Hide navbar when scrolling down
+    } else {
+        nav.classList.remove("hidden"); // Show navbar when scrolling up
+    }
+    lastScrollTop = scrollTop;
+});
+
+    function changeImage() {
+        var image = document.getElementById("imgClickAndChange"); // Use the ID of the image to select it
+        if (image.src.includes("placeholder.jpg")) {
+            image.src = "me.jpg"; // Change to another image
+        } else {
+            image.src = "placeholder.jpg"; // Change back to the original image
+        }
+    }
+
+    function updateTime() {
+
+        const currentTime = new Date().toLocaleTimeString();
+  
+        document.getElementById('current-time').textContent = `Current Time: ${currentTime}`;
+      }
+  
+      // Initial time update
+      updateTime();
+  
+      // Update the time every second (1000 milliseconds)
+      setInterval(updateTime, 1000);
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+      
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+    
